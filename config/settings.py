3133745303572d5bab2c2901ce416a873321dd50
mohammadtout24 <mohammadtout24@gmail.com>
2025-12-31 14:11:13 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/files/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
