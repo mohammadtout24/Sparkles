eb11a0371fb2513b8fae534c75b7a57002b7e6c0
mohammadtout24 <mohammadtout24@gmail.com>
2025-12-31 16:52:39 +0200

    Add EMAIL_TIMEOUT setting to email configuration

--- config/settings.py
+++ config/settings.py
@@ -116,3 +116,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "rayansparkles@gmail.com")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_TIMEOUT = 10
+
